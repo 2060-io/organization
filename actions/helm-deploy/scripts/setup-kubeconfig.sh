ef6bb8aed097c64ef0bc7626fe429b6cd0f5b9f7
andres vallecilla <andresfelipe083195@hotmail.com>
2026-01-29 12:53:28 -0500

    fix: testing

--- actions/helm-deploy/scripts/setup-kubeconfig.sh
+++ actions/helm-deploy/scripts/setup-kubeconfig.sh
@@ -5,10 +5,14 @@ echo "[DEBUG] setup-kubeconfig.sh: start"
 MANIFEST="$1"
 CLUSTER_NAME="$2"
 
+echo "[DEBUG] CLUSTER_NAME: '$CLUSTER_NAME'"
+echo "[DEBUG] MANIFEST: '$MANIFEST'"
 CLUSTER_INDEX=$(yq e ".clusters | map(.name == \"$CLUSTER_NAME\") | index(true)" "$MANIFEST")
 [ "$CLUSTER_INDEX" = "null" ] && echo "::error::Cluster not found" && exit 1
+echo "[DEBUG] CLUSTER_INDEX: $CLUSTER_INDEX"
 
 KUBECONFIG_SECRET=$(yq e ".clusters[$CLUSTER_INDEX].kubeconfigSecret" "$MANIFEST")
+echo "[DEBUG] KUBECONFIG_SECRET: $KUBECONFIG_SECRET"
 KUBECONFIG_VALUE="${!KUBECONFIG_SECRET:-}"
 [ -z "$KUBECONFIG_VALUE" ] && echo "::error::Missing kubeconfig" && exit 1
 
